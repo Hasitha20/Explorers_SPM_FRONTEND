ecff0b2ff2a2e4f3a4c01aa92e984fc7d5a68f3b
Lahiru Sanjeewa <it19166834@my.sliit.lk>
2021-09-02 02:10:05 +0530

    Create side nav

---
+++ SPM_explores_CAFE_frontend/src/components/header/manager/SideBar.js
@@ -0,0 +1,89 @@
+import React, {useState, useContext} from 'react'
+import styled from 'styled-components'
+//import { GlobalState } from '../../../GlobalState'
+import {Link} from 'react-router-dom'
+import * as FaIcons from 'react-icons/fa'
+import {SidebarData} from './SidebarData'
+import SubMenu from './SubMenu'
+//import axios from 'axios'
+
+const Nav = styled.div`
+    background: #15171c;
+    height: 80px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const NavIcon = styled(Link)`
+    margin-left: 2rem;
+    font-size: 2rem;
+    height: 80px;
+    display: flex;
+    justify-content: flex-start;
+    align-item: center;
+    margin-top: 30px;
+`;
+
+const SideBarNav = styled.nav`
+    background: #15171c;
+    width: 300px;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+    transition: 350ms;
+    z-index: 10;
+`;
+
+const SidebarWrap = styled.div`
+    width: 100%;
+`;
+
+export default function SideBar() {
+    
+
+    const [sidebar, setSidebar] = useState(true)
+
+    const showSidebar = () => setSidebar(!sidebar)
+
+    const notLoggedRouter = () => {
+        return (
+            
+                <div style={{paddingTop: "20px"}}> 
+                    {SidebarData.map((item, index) => {
+                    return <SubMenu item={item} key={index} />;
+                    })}
+                </div>
+            
+        )
+    }
+
+    return (
+        <div>
+        <Nav>
+            <NavIcon to="#">
+                <FaIcons.FaBars onClick={showSidebar} />
+            </NavIcon>
+        </Nav>
+
+        <SideBarNav sidebar={sidebar} style={{fontSize: "20px"}}>
+            <SidebarWrap>
+                <NavIcon to="/" style={{color: "white", textDecoration: "none"}}>
+                    Explores CAFE
+                </NavIcon>
+                <div> 
+                    {SidebarData.map((item, index) => {
+                    return <SubMenu item={item} key={index} />;
+                    })}
+                </div>
+                
+            </SidebarWrap>
+        </SideBarNav>
+        </div>
+    )
+}
+
+
