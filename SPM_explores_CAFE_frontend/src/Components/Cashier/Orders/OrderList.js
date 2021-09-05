8d76a272c9b03a5662f44b7e94ca98e011ebd90b
Hiruni <malshikahiruni99@gmail.com>
2021-09-06 02:14:13 +0530

    issues fixed

--- SPM_explores_CAFE_frontend/src/Components/Cashier/Orders/OrderList.js
+++ SPM_explores_CAFE_frontend/src/Components/Cashier/Orders/OrderList.js
@@ -4,6 +4,8 @@ function OrderList(){
     return (
         <div className="home" style={{paddingLeft:"310px"}}>
             <h1>Order List</h1>
+
+            <table></table>
         </div>
     );
 }
